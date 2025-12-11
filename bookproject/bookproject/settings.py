a0f738f819940b8b4f9dbdc5832e34c2e6e83e23
amano02 <salt27758@gmail.com>
2025-12-11 13:45:17 +0900

    2025/12/11 13:45 写真の追加まで

--- bookproject/bookproject/settings.py
+++ bookproject/bookproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'book.apps.BookConfig'
+    'book.apps.BookConfig',
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -118,7 +119,14 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL='/media/'
+
+MEDIA_ROOT=BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "index"
